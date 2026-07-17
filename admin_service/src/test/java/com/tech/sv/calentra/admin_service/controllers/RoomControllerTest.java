8b9b6ccfa8a5eedbb2788128ea40890721c6b8c2
puthvattanangeen <ngeenputhvattana2026@gmail.com>
2026-07-17 16:10:12 +0700

    17.07.2026_valio_services

---
+++ admin_service/src/test/java/com/tech/sv/calentra/admin_service/controllers/RoomControllerTest.java
@@ -0,0 +1,9 @@
+//package com.tech.sv.calentra.admin_service.controllers;
+//
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//
+//@WebMvcTest(RoomController.class)
+//class RoomControllerTest {
+//
+//    	
+//}
