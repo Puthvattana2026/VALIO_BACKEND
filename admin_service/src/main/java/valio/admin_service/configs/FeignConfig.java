81abd7bfa32180f6b67c0e28b263adb05a0dd494
puthvattanangeen <ngeenputhvattana2026@gmail.com>
2026-07-26 20:13:14 +0700

    26.07.2026_service_to_services

---
+++ admin_service/src/main/java/valio/admin_service/configs/FeignConfig.java
@@ -0,0 +1,34 @@
+package valio.admin_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+
+        return template -> {
+
+            ServletRequestAttributes attributes =
+                    (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+            if (attributes == null)
+                return;
+
+            HttpServletRequest request = attributes.getRequest();
+
+            String token = request.getHeader("Authorization");
+
+            if (token != null) {
+                template.header("Authorization", token);
+            }
+        };
+    }
+}
