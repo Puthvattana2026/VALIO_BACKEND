7b350bfb33431506395f5d5afe21dad012f1c2ec
puthvattanangeen <ngeenputhvattana2026@gmail.com>
2026-08-03 00:07:00 +0700

    02.08.2026_guest_implement

--- guest_service/src/main/java/valio/guest_service/GuestServiceApplication.java
+++ guest_service/src/main/java/valio/guest_service/GuestServiceApplication.java
@@ -3,7 +3,6 @@ package valio.guest_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableEur
 @SpringBootApplication
 public class GuestServiceApplication {
 
