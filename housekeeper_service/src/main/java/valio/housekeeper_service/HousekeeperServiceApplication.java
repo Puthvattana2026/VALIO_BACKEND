441e8d88001f61109be346da9c67127d43709c11
puthvattanangeen <ngeenputhvattana2026@gmail.com>
2026-08-04 16:01:46 +0700

    04.08.2026_add_services

--- housekeeper_service/src/main/java/valio/housekeeper_service/HousekeeperServiceApplication.java
+++ housekeeper_service/src/main/java/valio/housekeeper_service/HousekeeperServiceApplication.java
@@ -1,9 +1,13 @@
 package valio.housekeeper_service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class HousekeeperServiceApplication {
 
 	public static void main(String[] args) {
--- staff_service/src/main/java/valio/staff_service/StaffServiceApplication.java
+++ staff_service/src/main/java/valio/staff_service/StaffServiceApplication.java
@@ -2,8 +2,11 @@ package valio.staff_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class StaffServiceApplication {
 
 	public static void main(String[] args) {
